02874b828c491f7bc5e846a94d8c78c46207d053
Kazim-Mohammadi <kazimmohammadi400@gmail.com>
2021-09-04 21:29:11 +0430

    All migrations,models,controllers, and thier coresponded views created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ChildCategoryController;
+use App\Http\Controllers\Admin\CityController;
+use App\Http\Controllers\Admin\CountryController;
+use App\Http\Controllers\Admin\StateController;
+use App\Http\Controllers\Admin\SubCategoryController;
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +27,14 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::resource('categories', CategoryController::class);
+Route::resource('subcategories', SubCategoryController::class);
+Route::resource('childcategories', ChildCategoryController::class);
+Route::resource('countries', CountryController::class);
+Route::resource('states', StateController::class);
+Route::resource('cities', CityController::class);
+
+
+Route::get('listings/create', [ListingController::class, 'create'])->name('listings.create');
+Route::post('listings/store', [ListingController::class, 'store'])->name('listings.store');
